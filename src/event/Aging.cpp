4f461861d49e2c95b872749fec9915f3fabb5984
Matthew Carroll <matthew.j.carroll@outlook.com>
2023-08-31 09:44:28 -0400

    Aug21 tests (#8)
    * Quick Test Updates
    
    * Implement Aging
    
    * silly age needs to be a double
    
    * Addressing comments on PR

---
+++ src/event/Aging.cpp
@@ -0,0 +1,10 @@
+#include "Aging.hpp"
+
+namespace Event {
+    void Aging::doEvent(Person::Person &person) {
+        if (person.getIsAlive()) {
+            double monthAge = 1.0 / 12.0;
+            person.age += monthAge;
+        }
+    }
+} // namespace Event
--- src/event/Death.cpp
+++ src/event/Death.cpp
@@ -1,5 +1,16 @@
 #include "Death.hpp"
 
 namespace Event {
-    void Death::doEvent(Person::Person &person) {}
+    void Death::doEvent(Person::Person &person) {
+        if (person.age >= 1200) {
+            this->die(person);
+        }
+
+        // "Calculate background mortality rate based on age, gender, and IDU"
+        double backgroundMortality = 1.0;
+    }
+
+    void Death::die(Person::Person &person) {
+        // do the death thing here
+    }
 } // namespace Event
