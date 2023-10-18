6a72fc91a7f8a7b6230b191a4d00c050f0643b9e
Matthew Carroll <matthew.j.carroll@outlook.com>
2023-10-18 09:46:21 -0400

    Adding table to constructor

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "Aging.hpp"
 #include "BehaviorChanges.hpp"
 #include "Clearance.hpp"
+#include "DataTable.hpp"
 #include "DiseaseProgression.hpp"
 #include "Infections.hpp"
 #include "Person.hpp"
@@ -9,21 +10,22 @@
 
 int main(int argc, char *argv[]) {
     Simulation sim(0, 0);
+    Data::DataTable table;
 
     std::vector<std::shared_ptr<Event::Event>> personEvents;
     std::shared_ptr<Event::Event> aging = std::make_shared<Event::Aging>();
     personEvents.push_back(aging);
     std::shared_ptr<Event::Event> behavior =
-        std::make_shared<Event::BehaviorChanges>(sim.getGenerator());
+        std::make_shared<Event::BehaviorChanges>(sim.getGenerator(), table);
     personEvents.push_back(behavior);
     std::shared_ptr<Event::Event> clearance =
-        std::make_shared<Event::Clearance>(sim.getGenerator());
+        std::make_shared<Event::Clearance>(sim.getGenerator(), table);
     personEvents.push_back(clearance);
     std::shared_ptr<Event::Event> infection =
-        std::make_shared<Event::Infections>(sim.getGenerator());
+        std::make_shared<Event::Infections>(sim.getGenerator(), table);
     personEvents.push_back(infection);
     std::shared_ptr<Event::Event> disease =
-        std::make_shared<Event::DiseaseProgression>(sim.getGenerator());
+        std::make_shared<Event::DiseaseProgression>(sim.getGenerator(), table);
     personEvents.push_back(disease);
 
     sim.loadEvents(personEvents);
