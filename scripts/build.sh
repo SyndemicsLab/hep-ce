f5ba58c914b33607786929f16e9f1b38164ac683
Dimitri Baptiste <55843498+ddbaptiste@users.noreply.github.com>
2025-03-06 09:27:28 -0500

    Screening and Linking Unit Tests (#74)
    * Adding and refining tests to linking while attempting to determine why no linkage is occurring in DETECT HEPCEv2 validation.
    * Updating progress with unit tests while trying to debug linking.
    * Make `Screening` tests more specific; account for all scenarios around deciding whether linking happens.
    * Restore `build.sh` to former state.

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,15 @@ if command -v module &>/dev/null; then
     module load cmake/3.22.2
 fi
 
+# red output
+RED='\033[0;31m'
+# no color
+NC='\033[0m'
+
+err () {
+    echo -e "$RED$1$NC"
+}
+
 # help message to be output either with the -h flag or when using invalid syntax
 showhelp () {
     printf "\033[31m%s\033[0m" "$0 - Build the HEP-CE Model"
