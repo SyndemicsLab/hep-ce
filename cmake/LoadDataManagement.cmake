fc9237bad82e550937538712ea943809d9183335
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-05-12 17:04:11 -0400

    testing with a new DM

--- cmake/LoadDataManagement.cmake
+++ cmake/LoadDataManagement.cmake
@@ -3,7 +3,7 @@ include(FetchContent)
 FetchContent_Declare(
     datamanagement
     GIT_REPOSITORY https://github.com/SyndemicsLab/DataManagement.git
-    GIT_TAG main
+    GIT_TAG hotfix/sqlitecpp-install
 )
 option(DATAMANAGEMENT_INSTALL "Enable install for datamanagement project" ON)
 option(DATAMANAGEMENT_BUILD_TESTS "Disable testing for DataManagement" OFF)
