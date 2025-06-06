15afc313880cfdf942e3280b60f38986b0dacd9b
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-06-06 11:36:17 -0400

    Addressing PR comments

--- src/event/internals/treatment_internals.hpp
+++ src/event/internals/treatment_internals.hpp
@@ -4,7 +4,7 @@
 // Created Date: 2025-04-18                                                  //
 // Author: Matthew Carroll                                                    //
 // -----                                                                      //
-// Last Modified: 2025-06-04                                                  //
+// Last Modified: 2025-06-06                                                  //
 // Modified By: Matthew Carroll                                               //
 // -----                                                                      //
 // Copyright (c) 2025 Syndemics Lab at Boston Medical Center                  //
@@ -140,8 +140,6 @@ protected:
     /// @param
     /// @return
     inline bool LostToFollowUp(model::Person &person, model::Sampler &sampler) {
-        // If the person is already on treatment, they can't be lost to
-        // follow up
         if ((sampler.GetDecision({_ltfu_probability[person.GetPregnancyDetails()
                                                         .pregnancy_state]}) ==
              0)) {
