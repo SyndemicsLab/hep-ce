d286649860b6a73deb417691feac15a8aea2d47c
Andrew Clark <aclark8529@gmail.com>
2025-07-14 15:03:35 -0400

    fix to IsEligibleLastTimeActive

--- src/event/internals/treatment_internals.hpp
+++ src/event/internals/treatment_internals.hpp
@@ -220,9 +220,9 @@ private:
     }
 
     bool IsEligibleTimeLastActive(const model::Person &person) const {
-        auto time = person.GetCurrentTimestep() -
-                    person.GetBehaviorDetails().time_last_active;
-        if (time <= -1 || time > _eligibilities.time_since_last_use) {
+        auto time = person.GetBehaviorDetails().time_last_active;
+        auto time_diff = person.GetCurrentTimestep() - time;
+        if (time <= -1 || time_diff > _eligibilities.time_since_last_use) {
             return true;
         }
         return false;
