0f8a63751ea2d61f402e4f18159688d6d9fdd8ea
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2025-10-10 14:52:20 -0400

    Adding MOUD Event per PreVenT diagram

--- tests/src/event/behavior/moud_test.cpp
+++ tests/src/event/behavior/moud_test.cpp
@@ -4,7 +4,7 @@
 // Created Date: 2025-05-01                                                   //
 // Author: Matthew Carroll                                                    //
 // -----                                                                      //
-// Last Modified: 2025-10-09                                                  //
+// Last Modified: 2025-10-10                                                  //
 // Modified By: Matthew Carroll                                               //
 // -----                                                                      //
 // Copyright (c) 2025 Syndemics Lab at Boston Medical Center                  //
@@ -32,8 +32,10 @@
 #include <inputs_db.hpp>
 #include <person_mock.hpp>
 #include <sampler_mock.hpp>
+#include <utility.hpp>
 
 using ::testing::_;
+using ::testing::NiceMock;
 using ::testing::Return;
 
 namespace hepce {
@@ -41,14 +43,11 @@ namespace testing {
 
 class MoudTest : public ::testing::Test {
 protected:
-    MockPerson mock_person;
+    NiceMock<MockPerson> mock_person;
     MockSampler mock_sampler;
     std::string test_db = "inputs.db";
     std::string test_conf = "sim.conf";
     std::unique_ptr<datamanagement::ModelData> model_data;
-    double discounted_cost;
-    double discounted_life;
-    data::BehaviorDetails behaviors = {data::Behavior::kInjection, 0};
     data::HCVDetails hcv = {data::HCV::kNone,
                             data::FibrosisState::kF0,
                             false,
@@ -61,17 +60,17 @@ protected:
     data::ScreeningDetails screen = {-1, 0, 0, false, false, -1, 0};
 
     void SetUp() override {
-        ExecuteQueries(
-            test_db, {{"DROP TABLE IF EXISTS hcv_impacts;", CreateHCVImpacts(),
-                       "INSERT INTO hcv_impacts "
-                       "VALUES (0, 0, 230.65, 0.915);",
-                       "INSERT INTO hcv_impacts "
-                       "VALUES (1, 0, 430.00, 0.8);",
-                       "INSERT INTO hcv_impacts "
-                       "VALUES (1, 1, 500.00, 0.7);"}});
+        ExecuteQueries(test_db, {{"DROP TABLE IF EXISTS moud_transitions;",
+                                  CreateMoudTransitions(),
+                                  "INSERT INTO moud_transitions VALUES (25, 1, "
+                                  "4, -1, 0.0, 1.0, 0.0);"
+                                  "INSERT INTO moud_transitions VALUES (25, 1, "
+                                  "8, -1, 0.0, 0.0, 1.0);",
+                                  "INSERT INTO moud_transitions VALUES (25, 0, "
+                                  "0, -1, 1.0, 0.0, 0.0);",
+                                  "INSERT INTO moud_transitions VALUES (30, 0, "
+                                  "0, -1, 0.0, 1.0, 0.0);"}});
         BuildSimConf(test_conf);
-        discounted_cost = utils::Discount(370.75, 0.0025, 1, false);
-        discounted_life = utils::Discount(1, 0.0025, 1, false);
         model_data = datamanagement::ModelData::Create(test_conf);
         model_data->AddSource(test_db);
     }
@@ -81,5 +80,168 @@ protected:
         std::filesystem::remove(test_conf);
     }
 };
+
+TEST_F(MoudTest, PersonIsDead) {
+    // Setup
+    const std::string LOG_NAME = "PersonIsDead";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current;
+
+    // Expectations
+    EXPECT_CALL(mock_person, IsAlive()).WillOnce(Return(false));
+    EXPECT_CALL(mock_person, GetBehaviorDetails()).Times(0);
+    EXPECT_CALL(mock_person, GetMoudDetails()).Times(0);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, NoHistoryOfOUD) {
+    // Setup
+    const std::string LOG_NAME = "NoHistoryOfOUD";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current;
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+
+    // Expectations
+    EXPECT_CALL(mock_person, GetMoudDetails()).Times(0);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, PostTreatment) {
+    // Setup
+    const std::string LOG_NAME = "PostTreatment";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current = {data::Behavior::kInjection, 0};
+    data::MOUDDetails moud_details = {data::MOUD::kPost, -1, 1, 1};
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+    ON_CALL(mock_person, GetMoudDetails()).WillByDefault(Return(moud_details));
+
+    // Expectations
+    EXPECT_CALL(mock_person, TransitionMOUD()).Times(1);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, StayOnTreatment) {
+    // Setup
+    const std::string LOG_NAME = "StayOnTreatment";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current = {data::Behavior::kInjection, 0};
+    data::MOUDDetails moud_details = {data::MOUD::kCurrent, 0, 4, 4};
+
+    std::vector<double> expected_probs = {0.0, 1.0, 0.0};
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+    ON_CALL(mock_person, GetMoudDetails()).WillByDefault(Return(moud_details));
+    ON_CALL(mock_person, GetAge()).WillByDefault(Return(300));
+
+    // Expectations
+    EXPECT_CALL(mock_sampler, GetDecision(expected_probs)).WillOnce(Return(1));
+    EXPECT_CALL(mock_person, TransitionMOUD()).Times(0);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, StopTreatment) {
+    // Setup
+    const std::string LOG_NAME = "StopTreatment";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current = {data::Behavior::kInjection, 0};
+    data::MOUDDetails moud_details = {data::MOUD::kCurrent, 0, 8, 8};
+
+    std::vector<double> expected_probs = {0.0, 0.0, 1.0};
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+    ON_CALL(mock_person, GetMoudDetails()).WillByDefault(Return(moud_details));
+    ON_CALL(mock_person, GetAge()).WillByDefault(Return(300));
+
+    // Expectations
+    EXPECT_CALL(mock_sampler, GetDecision(expected_probs)).WillOnce(Return(2));
+    EXPECT_CALL(mock_person, TransitionMOUD()).Times(1);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, StartTreatment) {
+    // Setup
+    const std::string LOG_NAME = "StartTreatment";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current = {data::Behavior::kInjection, 0};
+    data::MOUDDetails moud_details = {data::MOUD::kNone, 0, 0, 0};
+
+    std::vector<double> expected_probs = {0.0, 1.0, 0.0};
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+    ON_CALL(mock_person, GetMoudDetails()).WillByDefault(Return(moud_details));
+    ON_CALL(mock_person, GetAge()).WillByDefault(Return(360));
+
+    // Expectations
+    EXPECT_CALL(mock_sampler, GetDecision(expected_probs)).WillOnce(Return(1));
+    EXPECT_CALL(mock_person, TransitionMOUD()).Times(1);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
+TEST_F(MoudTest, StayOffTreatment) {
+    // Setup
+    const std::string LOG_NAME = "StayOffTreatment";
+    CreateTestLog(LOG_NAME);
+    data::BehaviorDetails behavior_current = {data::Behavior::kInjection, 0};
+    data::MOUDDetails moud_details = {data::MOUD::kNone, 0, 4, 2};
+
+    ON_CALL(mock_person, IsAlive()).WillByDefault(Return(true));
+    ON_CALL(mock_person, GetBehaviorDetails())
+        .WillByDefault(Return(behavior_current));
+    ON_CALL(mock_person, GetMoudDetails()).WillByDefault(Return(moud_details));
+    ON_CALL(mock_person, GetAge()).WillByDefault(Return(300));
+
+    // Expectations
+    EXPECT_CALL(mock_sampler, GetDecision(_)).WillOnce(Return(2));
+    EXPECT_CALL(mock_person, TransitionMOUD()).Times(0);
+
+    // Run test
+    auto event = event::behavior::Moud::Create(*model_data, LOG_NAME);
+    event->Execute(mock_person, mock_sampler);
+
+    RemoveTestLog(LOG_NAME);
+}
+
 } // namespace testing
 } // namespace hepce
