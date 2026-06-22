1a6a1e34f37b0e513edc405b35a47d242af4f1e0
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-22 16:42:56 -0400

    Maybe now we can time out the actions with building

--- cmake/LoadBoost.cmake
+++ cmake/LoadBoost.cmake
@@ -3,15 +3,15 @@ list(APPEND CMAKE_MESSAGE_INDENT "  ")
 
 set(BOOST_ENABLE_CMAKE ON)
 
+message(STATUS "Downloading and extracting boost library sources. This will take some time...")
 FetchContent_Declare(
     Boost
-    GIT_REPOSITORY https://github.com/boostorg/boost.git
-    GIT_TAG 1bed2b0712b2119f20d66c5053def9173c8462a5 # v1.90.0
-    GIT_SHALLOW TRUE
-    EXCLUDE_FROM_ALL TRUE
+    URL https://github.com/boostorg/boost/releases/download/boost-1.91.0-1/boost-1.91.0-1-cmake.7z # downloading a zip release speeds up the download
+    USES_TERMINAL_DOWNLOAD TRUE 
+    GIT_PROGRESS TRUE   
+    DOWNLOAD_NO_EXTRACT FALSE
     OVERRIDE_FIND_PACKAGE TRUE
 )
-
 
 list(POP_BACK CMAKE_MESSAGE_INDENT)
 message(CHECK_PASS "Boost Fetched")
