db300d4d79aee164846f9b5b3aed423f8e7fae84
Matthew Carroll <28577806+MJC598@users.noreply.github.com>
2026-06-22 17:26:17 -0400

    reverting to old boost find_package

--- cmake/MakeDependenciesAvailable.cmake
+++ cmake/MakeDependenciesAvailable.cmake
@@ -1,13 +1,12 @@
 include(FetchContent)
 
-include(${PRIVATE_MODULE_PATH}/LoadBoost.cmake)
 include(${PRIVATE_MODULE_PATH}/LoadSpdlog.cmake)
 include(${PRIVATE_MODULE_PATH}/LoadSQLiteCpp.cmake)
 
 if(HEPCE_BUILD_TESTS STREQUAL "ON")
     include(${PRIVATE_MODULE_PATH}/LoadGtest.cmake)
-    FetchContent_MakeAvailable(Boost SQLiteCpp spdlog googletest)
+    FetchContent_MakeAvailable(SQLiteCpp spdlog googletest)
     include(GoogleTest)
 elseif(HEPCE_BUILD_TESTS STREQUAL "OFF")
-    FetchContent_MakeAvailable(Boost SQLiteCpp spdlog)
+    FetchContent_MakeAvailable(SQLiteCpp spdlog)
 endif()
